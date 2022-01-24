c2aee79aac17e31f75bf4666734861ff1b5c0130
Katie <costantinkp@gmail.com>
2022-01-24 15:56:19 -0500

    chang app from class to function component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,91 @@
-import React, { Component } from 'react';
-
+import React, { useState } from 'react';
 import CharPicker from './components/CharPicker';
 import Character from './components/Character';
 
-class App extends Component {
-  state = {
+const App = () => {
+
+  const [state, setState] =useState({
     selectedCharacter: 1,
     side: 'light',
     destroyed: false
-  };
+  })
 
-  sideHandler = side => {
-    this.setState({ side: side });
-  };
+  const sideHandler = side => {
+      setState({ ...state, side: side });
+    };
+    //use spread here to change just the side and not override entire state
 
-  charSelectHandler = event => {
-    const charId = event.target.value;
-    this.setState({ selectedCharacter: charId });
-  };
+  const charSelectHandler = event => {
+      const charId = event.target.value;
+      //console.log(event.target.value);
+      setState({ ...state, selectedCharacter: charId });
+    };
 
-  destructionHandler = () => {
-    this.setState({ destroyed: true });
-  };
+  const destructionHandler = () => {
+      setState({ ...state, destroyed: true });
+    };
 
-  render() {
     return (
-      <React.Fragment>
+      <>
         <CharPicker
-          side={this.state.side}
-          selectedChar={this.state.selectedCharacter}
-          onCharSelect={this.charSelectHandler}
+          side={state.side}
+          selectedChar={state.selectedCharacter}
+          onCharSelect={charSelectHandler}
         />
-        <Character selectedChar={this.state.selectedCharacter} />
-        <button onClick={this.sideHandler.bind(this, 'light')}>
+        <Character selectedChar={state.selectedCharacter} />
+        <button onClick={sideHandler.bind(this, 'light')}>
           Light Side
         </button>
-        <button onClick={this.sideHandler.bind(this, 'dark')}>Dark Side</button>
-        {this.state.side === 'dark' && (
-          <button onClick={this.destructionHandler}>DESTROY!</button>
+        <button onClick={sideHandler.bind(this, 'dark')}>Dark Side</button>
+        {state.side === 'dark' && (
+          <button onClick={destructionHandler}>DESTROY!</button>
         )}
-      </React.Fragment>
+      </>
     );
-
-  }
-}
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     selectedCharacter: 1,
+//     side: 'light',
+//     destroyed: false
+//   };
+
+//   sideHandler = side => {
+//     this.setState({ side: side });
+//   };
+
+//   charSelectHandler = event => {
+//     const charId = event.target.value;
+//     this.setState({ selectedCharacter: charId });
+//   };
+
+//   destructionHandler = () => {
+//     this.setState({ destroyed: true });
+//   };
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <CharPicker
+//           side={this.state.side}
+//           selectedChar={this.state.selectedCharacter}
+//           onCharSelect={this.charSelectHandler}
+//         />
+//         <Character selectedChar={this.state.selectedCharacter} />
+//         <button onClick={this.sideHandler.bind(this, 'light')}>
+//           Light Side
+//         </button>
+//         <button onClick={this.sideHandler.bind(this, 'dark')}>Dark Side</button>
+//         {this.state.side === 'dark' && (
+//           <button onClick={this.destructionHandler}>DESTROY!</button>
+//         )}
+//       </React.Fragment>
+//     );
+
+//   }
+// }
+
+// export default App;
